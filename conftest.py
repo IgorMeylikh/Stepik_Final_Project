1887c53b1289753adacf24fb50e36a2eab5b17c6
IgorMeylikh <dream_killer2007@mail.ru>
2021-11-23 14:39:37 +0300

    После загрузки в PyCharm

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ def pytest_addoption(parser): #парсим язык
                     help="Choose language")
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("language")
     options = Options()
